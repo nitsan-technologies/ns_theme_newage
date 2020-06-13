77276718fc7208fd18d99e74e50c9194774e2c0e
NITSAN Technologies <sanjay@nitsan.in>
2020-06-13 15:14:51 +0530

    [TODO] Ready to Release v2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,10 +2,10 @@
 
 // Provide detailed information and depenencies of EXT:ns_theme_newage
 $EM_CONF['ns_theme_newage'] = [
-    'title' => '[NITSAN] New Age',
+    'title' => '[NITSAN] New Age Mobile TYPO3 Template',
     'description' => 'T3 New Age Landing TYPO3 Template is a Bootstrap minimal app landing page. This template has been designed for any need to showcase app or website, app landing page. Live-Demo: https://demo.t3terminal.com/?theme=t3t-newage Pro-Support: https://t3terminal.com/t3-new-age-landing-typo3-template',
     'category' => 'templates',
-    'author' => 'NITSAN Technologies Pvt Ltd',
+    'author' => 'T3: Sonal Chauhan, QA: Siddharth Sheth',
     'author_email' => 'info@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
