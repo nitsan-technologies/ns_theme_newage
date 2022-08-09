4d2c2b9bfad7ad86ebf0da1dc2255a94b40714d0
NITSAN Technologies <sanjay@nitsan.in>
2022-08-09 17:11:43 +0530

    [TASK] Change URL of t3terminal into t3planet

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 // Provide detailed information and depenencies of EXT:ns_theme_newage
 $EM_CONF['ns_theme_newage'] = [
 	'title' => '[NITSAN] T3 New Age TYPO3 Template',
-	'description' => 'T3 New Age Landing TYPO3 Template is a Bootstrap minimal app landing page. This template has been designed for any need to showcase app or website, app landing page. Built it with twitter bootstrap 3, HTML5, CSS3 and SASS. Live-Demo: https://demo.t3terminal.com/?theme=t3t-newage Pro-Version & Support: https://t3terminal.com/t3-new-age-landing-typo3-template',
+	'description' => 'T3 New Age Landing TYPO3 Template is a Bootstrap minimal app landing page. This template has been designed for any need to showcase app or website, app landing page. Built it with twitter bootstrap 3, HTML5, CSS3 and SASS. Live-Demo: https://demo.t3planet.com/?theme=t3t-newage Pro-Version & Support: https://t3planet.com/t3-new-age-landing-typo3-template',
 	'category' => 'templates',
 	'author' => 'Team NITSAN',
 	'author_email' => 'info@nitsan.in',
