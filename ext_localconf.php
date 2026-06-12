9562cb46b8c0befbb68b09f60003e493478897fe
heli-bhadiyadra <heli.bhadiyadra@mail.nitsan.ai>
2026-06-12 17:18:49 +0530

    [TASK] Install and configure visual editor

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-$_EXTKEY = "ns_theme_newage";
+
+defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:ns_theme_newage/Configuration/RTE/Default.yaml';
-
