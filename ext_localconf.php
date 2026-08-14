f27ae825945f9f2b0d8509750a5229e5d12ef2cd
nitsan-technologies <sanjay@nitsantech.com>
2026-08-14 16:23:17 +0530

    [TASK] Bugfixing

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,5 +5,3 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:ns_theme_newage/
 
 $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['ns_theme_newage'] =
     'EXT:ns_theme_newage/Resources/Public/Backend/Css/Backend.css';
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:ns_theme_newage/Configuration/PageTSconfig/setup.typoscript'");
